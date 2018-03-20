c1ae758c25eebfab82e771e1bce8e5f93cca1751
Vladimir Kukushkin <kukushechkin@mac.com>
2018-03-20 21:14:54 +0300

    Initial Commit

---
+++ YetAnotherProcessManager/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  YetAnotherProcessManager
+//
+//  Created by Vladimir Kukushkin on 3/20/18.
+//  Copyright © 2018 kukushechkin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
